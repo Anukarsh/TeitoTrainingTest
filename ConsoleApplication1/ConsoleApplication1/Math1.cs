31d9d2a5e1d8d6957aecee4457faa9faec8e2413
Anukarsh Prasad <anukarsh123@gmail.com>
2017-01-10 13:10:19 +0530

    Adding ConsoleApplication

---
+++ ConsoleApplication1/ConsoleApplication1/Math1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Math1
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject1/MathTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class MathTest
+    {
+        [TestMethod]
+        public void AddOneAndTwoGivesThree()
+        {
+            Math1 math = new Math1();
+
+            int actual = math.Add(1, 2);
+
+            Assert.AreEqual(3, actual, "Result not equal to 3");
+        }
+
+        [TestMethod]
+        public void AddOneAndTwoGivesFour()
+        {
+            Math1 math = new Math1();
+            int actual = math.Add(1, 2);
+            Assert.AreEqual(4, actual,"Result is not equal to 4");
+        }
+
+    }
+}
